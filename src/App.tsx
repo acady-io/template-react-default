b98f8af9d1ac361e69aa120ad0a633da3fbd70b9
schab <christian.schab@wusoma.com>
2021-01-09 11:35:02 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import acady from './acady.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h2>You just made a <a href="https://reactjs.org" target="_blank" className="App-link">React</a> app with the help of <a href="https://acady.io/" className="App-link">acady</a>!</h2>
+        <img src={acady} alt="acady" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
