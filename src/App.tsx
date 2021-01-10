e8b9dfa4a37fdcc4bb44de35fbe4b22f081cc08f
schab <christian.schab@wusoma.com>
2021-01-10 12:15:52 +0100

    Add acady

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,27 @@ import acady from './acady.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h2>You just made a <a href="https://reactjs.org" target="_blank" className="App-link">React</a> app with the help of <a href="https://acady.io/" className="App-link">acady</a>!</h2>
-        <img src={acady} alt="acady" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h2>
+                    You just made a <a href="https://reactjs.org" target="_blank" className="App-link" rel="noreferrer">React</a> app with the help of <a href="https://acady.io/" className="App-link" rel="noreferrer">acady</a>!
+                </h2>
+                <img src={acady} alt="acady"/>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
